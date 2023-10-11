f8422c6ad305c16571c5d60a631af5e4f19028bf
Em404 <emanuele.macchiarulo.1998@gmail.com>
2023-10-11 18:51:14 +0200

    modifiche

--- m1/s3/g3/asset/js/g3.js
+++ m1/s3/g3/asset/js/g3.js
@@ -1,43 +1,44 @@
 let buttonSaveItem = document.getElementById("save-item");
 
 buttonSaveItem.addEventListener("click", function () {
-  let target = document.getElementById("ol-target");
-  let targetDone = document.getElementById("ol-targetdone");
+  let targetListToDo = document.getElementById("ol-target");
+  let targetListDone = document.getElementById("ol-targetdone");
   let input = document.getElementById("item");
 
   let li = document.createElement("li"); //<li></li>
   li.classList.add("list-item"); //<li class="list-item"></li>
   // li.innerText = input.value; //<li class="list-item">testo</li>
 
-  let button = document.createElement("button");
-  button.classList.add("delete-item");
-  button.setAttribute("id", "delete-item");
-  button.innerText = "Elimina";
+  let buttonDelete = document.createElement("button");
+  buttonDelete.classList.add("delete-item");
+  buttonDelete.setAttribute("id", "delete-item");
+  buttonDelete.innerText = "Elimina";
 
-  let divButton = document.createElement("div");
   let divText = document.createElement("div");
+  let divButton = document.createElement("div");
+  divButton.classList.add("delete-button")
 
   if (!input.value) return; //se il campo è vuoto uso return per terminare la funzione
 
+  buttonDelete.addEventListener("click", function () {
+    console.log("clickato");
+    li.remove();
+  });
+  
   divText.addEventListener("click", function () {
     li.classList.add("done");
     divText.append(input.value);
-    divButton.append(button);
+    divButton.append(buttonDelete);
     li.append(divText, divButton);
-    targetDone.append(li);
+    targetListDone.append(li);
   });
 
   divText.append(input.value);
-  divButton.append(button);
+  divButton.append(buttonDelete);
   li.append(divText, divButton);
 
-  target.append(li); //inserisco l'elemento generato nel documento
-
+  targetListToDo.append(li); //inserisco l'elemento generato nel documento
+  
   input.value = ""; //svuoto l'input
-});
 
-// let buttonDeleteItem = document.getElementById("#delete-item");
-// console.log(buttonDeleteItem);
-// buttonDeleteItem.addEventListener("click", function () {
-//   console.log("clickato");
-// });
+});
