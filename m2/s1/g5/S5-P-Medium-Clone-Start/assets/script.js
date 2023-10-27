8048228f0e3b3bf17a828128ba7b96e9504db2b5
Em404 <emanuele.macchiarulo.1998@gmail.com>
2023-10-27 19:30:20 +0200

    fix css

--- m2/s1/g5/S5-P-Medium-Clone-Start/assets/script.js
+++ m2/s1/g5/S5-P-Medium-Clone-Start/assets/script.js
@@ -3,6 +3,7 @@ window.addEventListener("scroll",function(){
   let nav = document.querySelector('.wrap-nav');
   let button = document.querySelector('.item');
 
+  let sectionSticky = this.document.querySelector('.wrap-sf')
 
   if (top >= 400) {
       nav.classList.add('white');
@@ -10,6 +11,12 @@ window.addEventListener("scroll",function(){
   } else {
       nav.classList.remove('white');
       button.classList.remove('green');
+  }
+
+  if (top >= 800) {
+    sectionSticky.classList.add('sf-fix')
+  } else {
+    sectionSticky.classList.remove('sf-fix')
   }
 
 })
